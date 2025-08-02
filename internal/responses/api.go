eb119219f048605771b67c8357e0695ad4e1c207
Phanuphat Srisukhawasu <phanuphat.srisukhawasu@gmail.com>
2025-08-02 20:18:26 +0700

    feat(response): Implement API response structure

---
+++ internal/responses/api.go
@@ -0,0 +1,38 @@
+package responses
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
+
+type ApiResponse struct {
+	// Success respond will have Meta and Data fields
+	Meta *Meta `json:"meta,omitempty"`
+	Data any   `json:"data,omitempty"`
+	// Error respond will have Message field
+	Message string `json:"message,omitempty"`
+}
+
+func RespondSuccess(
+	ctx echo.Context,
+	meta *Meta,
+	data any,
+) error {
+	response := &ApiResponse{
+		Meta: meta,
+		Data: data,
+	}
+	return ctx.JSON(http.StatusOK, response)
+}
+
+func RespondError(
+	ctx echo.Context,
+	errorMessage string,
+	status int,
+) error {
+	response := &ApiResponse{
+		Message: errorMessage,
+	}
+	return ctx.JSON(status, response)
+}
